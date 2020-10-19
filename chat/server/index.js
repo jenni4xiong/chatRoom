da88a00540b5884c703976a4c330467fb74cac66
Jennifer Xiong <jenni4xiong@gmail.com>
2020-10-19 01:17:16 -0700

    RESTful routes

--- chat/server/index.js
+++ chat/server/index.js
@@ -4,6 +4,28 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/build')))
 
+app.get('/chats', (req, res) => {
 
+})
+
+app.get('/chats/:id/comments', (req, res) => {
+
+})
+
+app.post('/chats/:id/comments', (req, res) => {
+
+})
+
+app.post('/chats/:id', (req, res) => {
+
+})
+
+app.delete('/chats/:id', (req, res) => {
+
+})
+
+app.put('/chats/:id', (req, res) => {
+
+})
 
 app.listen(8080)
