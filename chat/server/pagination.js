2617d0b03d3bc1536a9ce41fef096b3e2791130b
Jennifer Xiong <jenni4xiong@gmail.com>
2020-10-24 00:20:57 -0700

    pagination

---
+++ chat/server/pagination.js
@@ -0,0 +1,21 @@
+const paginatedResults = (model) => {
+  return async (req, res, next) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+    const sortBy = req.query.sortBy;
+    const order = req.query.order;
+
+    const startIndex = (page - 1) * limit;
+
+    console.log('LIMIT', limit)
+    if (sortBy) {
+      res.paginatedResults = await model.find().sort({ [sortBy]: parseInt(order) }).limit(limit).skip(startIndex).exec()
+    } else {
+      res.paginatedResults = await model.find().limit(limit).skip(startIndex).exec()
+    }
+    res.totalCount = await model.countDocuments().exec();
+    next();
+  }
+}
+
+module.exports = paginatedResults;
